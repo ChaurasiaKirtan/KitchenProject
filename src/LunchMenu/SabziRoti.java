f01a5e8a8eb7a355e0e639963f3c6ce5478550cb
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-02-10 17:56:19 -0500

    third commit

--- src/LunchMenu/SabziRoti.java
+++ src/LunchMenu/SabziRoti.java
@@ -4,6 +4,8 @@ public class SabziRoti {
 
 	public static void main(String[] args) {
 		System.out.println("palak paneer");
+		System.out.println("matar paneer");
+		System.out.println("aloo paratha");
 
 	}
 
